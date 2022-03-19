671e8b3ef1e8db7a5178c28384b6074600ca5800
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-03-19 09:24:27 -0300

    Alteração das declarações de variávies de VAR para LET

--- JavaScript/1020_Idade_em_dias.js
+++ JavaScript/1020_Idade_em_dias.js
@@ -1,18 +1,18 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
+let input = require('fs').readFileSync('/dev/stdin', 'utf8');
+let lines = input.split('\n');
 //Declaração de variável com conversão explícita para int e método.lines.shift() 
 //para mudança da linha do arquivo de inputs do beecrowd
-var entrada = parseInt(lines.shift());
+let entrada = parseInt(lines.shift());
 //Declaração de variáveis sobre o ano, mês e dia, atribuição de resultado da expressão 
 //matemática e conversão explícita para int, respectivamente
-var ano = entrada / 365;
-var anoDisp = parseInt(ano);
+let ano = entrada / 365;
+let anoDisp = parseInt(ano);
 
-var mes = ((ano - anoDisp) * 365) / 30;
-var mesDisp = parseInt(mes);
+let mes = ((ano - anoDisp) * 365) / 30;
+let mesDisp = parseInt(mes);
 
-var dia = (mes - mesDisp) * 30;
-var diaDisp = Math.round(dia);
+let dia = (mes - mesDisp) * 30;
+let diaDisp = Math.round(dia);
 //Exibição formatada dos valores das variáveis anoDisp, mesDisp e diaDisp
 console.log(anoDisp + " ano(s)");
 console.log(mesDisp + " mes(es)");
